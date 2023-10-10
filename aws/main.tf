b6c07daa6df9a383586882d4059b1ce5875812c8
saurabhkothari <saurabhkothari@rocketmail.com>
2023-10-10 10:05:23 +0600

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -1,1 +1,17 @@
+# main.tf
 
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = "us-east-1" # Replace with your desired AWS region
+}
+
+resource "aws_instance" "example_instance" {
+  count         = var.instance_count
+  ami           = var.ami_id
+  instance_type = var.instance_type
+}
+
+output "instance_id" {
+  value = aws_instance.example_instance[*].id
+}
