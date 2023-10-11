3aa637aa26dfb43fd09de1ba32050783de79cc62
saurabhkothari <saurabhkothari@rocketmail.com>
2023-10-11 09:49:19 +0600

    Update output.tf

--- aws/output.tf
+++ aws/output.tf
@@ -1,9 +1,5 @@
 # output.tf
 
-output "instance_name" {
-  description = "Name tag of the EC2 instance"
-  value = aws_instance.example_instance[*].tags.Name
-}
 
 output "instance_ip" {
   description = "Public IP address of the EC2 instance"
