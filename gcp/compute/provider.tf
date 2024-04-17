401fb53c4baa019dd614b16c52da52c0e2fc398b
saurabhkothari <saurabhkothari@rocketmail.com>
2024-04-17 21:21:54 +0530

    Update provider.tf

--- gcp/compute/provider.tf
+++ gcp/compute/provider.tf
@@ -1,13 +1,11 @@
 terraform {
+  required_version = ">= 1.8.0"
   required_providers {
     google = {
       source = "hashicorp/google"
       version = "4.51.0"
     }
   }
-}
-
-terraform {
   cloud {
     hostname = "backend.api.env0.com"
     organization = "0b490cb7-f548-42b9-9ea0-acf8bfbf2375.75423f3c-5323-4aac-af95-416c5065cb4a"
