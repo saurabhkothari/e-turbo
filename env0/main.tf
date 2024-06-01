f409f55db5d00d2e5d7c9630fe1ce1fe13c2a4e8
Saurabh Kothari <kotharisaurabh11@gmail.com>
2024-06-01 12:55:52 +0530

    main.tf

---
+++ env0/main.tf
@@ -0,0 +1,14 @@
+resource "env0_gcp_gke_credentials" "credentials" {
+  name           = var.gke_cred_name
+  cluster_name   = var.gke_cluster_name
+  cluster_region = "us-central1"
+}
+
+data "env0_project" "project" {
+  name = "Turbo PoV"
+}
+
+resource "env0_cloud_credentials_project_assignment" "assignment" {
+  credential_id = env0_gcp_gke_credentials.credentials.id
+  project_id    = data.env0_project.project.id
+}
